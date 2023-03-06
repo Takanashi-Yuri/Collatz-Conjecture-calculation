84bc3f87122e861e0545f78ecd588b855f135286
Takanashi-Yuri <115999248+Takanashi-Yuri@users.noreply.github.com>
2023-03-06 16:43:33 +0100

    Updated program to more user-frindly style
    Added options for generating or manual entry of the numbers.
    Added printing of the output to the file.
    Output is now in sheet friendly style.

--- main.py
+++ main.py
@@ -1,34 +1,83 @@
-import  readline
-
-number1 = float(input("Please insert number > "))
+import readline
+from numpy import random
+question1 = input("Do you wish to generate random numbers (1) or input your own (2)? >")
 print()
-basenumber = number1
-count = 0
 
-f = open("final.txt", "a")
-f.write('\n')
-while number1 != 1:
-    if number1%2 == 0:
-        print(number1, "is even (/2)")
-        number1 = number1/2
-        print(number1, "=", basenumber, "/ 2")
-        write1 = str(number1) + " = " + str(basenumber) + " / 2"
-        f.write(write1)
-        f.write('\n')
+if question1 == "1":
+    range1 = input("Enter start of the range or leave the default one (1) >")
+    if range1 == "":
+        range1 = 1
     else:
-        print(number1, "is odd (3x+1)")
-        number1 = 3*number1+1
-        print(number1, "=", "3 *", basenumber, "+ 1")
-        write1 = str(number1) + " = " + "3 * " + str(basenumber) + " + 1"
-        f.write(write1)
-        f.write('\n')
-    basenumber = number1
-    count = count+1
+        range1 = int(range1)
+    range2 = input("Enter end of the range or leave the default one (1000000000) >")
+    if range2 == "":
+        range2 = 1000000000
+    else:
+        range2 = int(range2)
     print()
-print("Final count of runs is", count)
 
-write1 = 'Final count of runs is ' + str(count)
-f = open("final.txt", "a")
-f.write('\n')
-f.write(write1)
-f.close()
+    question2 = input("How many runs do you wish to perform? >")
+    runs1 = int(question2)
+    print()
+
+    f = open("output.txt", "a")
+    while runs1 != 0:
+        number1 = random.randint(range1, range2)
+        number2 = number1
+        basenumber = number1
+        count = 0
+
+        while number1 != 1:
+            if number1%2 == 0:
+                print(number1, "is even (/2)")
+                number1 = number1/2
+                print(number1, "=", number2, "/ 2")
+            else:
+                print(number1, "is odd (3x+1)")
+                number1 = 3*number1+1
+                print(number1, "=", "3 *", number2, "+ 1")
+            number2 = number1
+            count = count+1
+            print()
+        
+        write1 = 'Number '+ str(basenumber) + ' taken ' + str(count) + ' operations to get to 1.'
+        write2 = str(basenumber)+";"+str(count)
+        print(write1)
+        print()
+        f.write('\n')
+        f.write(write2)
+        runs1 = runs1-1
+    f.close()
+
+elif question1 == "2":
+    continue1 = "y"
+    while continue1 == "y":
+        number1 = float(input("Please insert number > "))
+        print()
+        number2 = number1
+        basenumber = number1
+        count = 0
+
+        while number1 != 1:
+            if number1%2 == 0:
+                print(number1, "is even (/2)")
+                number1 = number1/2
+                print(number1, "=", number2, "/ 2")
+            else:
+                print(number1, "is odd (3x+1)")
+                number1 = 3*number1+1
+                print(number1, "=", "3 *", number2, "+ 1")
+            number2 = number1
+            count = count+1
+            print()
+        
+        write1 = 'Number '+ str(basenumber) + ' taken ' + str(count) + ' operations to get to 1. (Sheet version: ' + str(basenumber)+";"+str(count) + ')'
+        print(write1)
+        print()
+
+        continue1 = input("Do you wish to enter another number? y/n >")
+        print()
+    print("Ending...")
+
+else:
+    print("Please enter only one of the valid options. Ending program...")
